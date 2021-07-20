98a0acb72100cfa5810313df41c194cc35ce998f
yoon-seul <66136653+yoon-seul@users.noreply.github.com>
2021-07-20 15:21:49 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+
+        int[] input = new int[10];
+        int count = 0;
+        int result =0;
+
+        for (int i = 0 ; i<10 ; i++ ){
+            input[i]=scan.nextInt();
+            input[i] = input[i] % 42;
+        }
+
+        for (int i=0;i<10;i++){
+            for (int j=i+1;j<10;j++){
+                if (input[i]==input[j]){ //한개만 있는 것을 센다
+                    count++;
+                }    
+            }
+            if (count==0){
+                result++;
+            }
+            
+        }
+
+        
+
+        System.out.printf("%d",result);
+    }
+}
