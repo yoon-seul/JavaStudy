e05f627b910ea3f4b752ebf8dff234450e15d4f5
yoon-seul <66136653+yoon-seul@users.noreply.github.com>
2021-07-21 18:00:31 +0900

    Add files via upload

---
+++ 2292.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void Main(String args[]){
+        Scanner scan = new Scanner(System.in);
+
+        int num = scan.nextInt();
+
+        
+    }
+}
