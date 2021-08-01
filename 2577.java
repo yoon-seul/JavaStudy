6ed13a47f9ae45cd55e7e3ffa5cfa166757396b0
yoon-seul <66136653+yoon-seul@users.noreply.github.com>
2021-08-01 21:14:21 +0900

    Add files via upload

---
+++ 2577.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+
+        int numA = scan.nextInt();
+        int numB = scan.nextInt();
+        int numC = scan.nextInt();
+
+        double[] num = new double[10];
+        int[] fResult = new int[10];
+
+        for (int i=0;i<10;i++){
+            num[i]=10;
+        }
+
+        double result = numA * numB * numC;
+
+        for (int i=0;i<10;i++){
+            num[i]= result % 10;
+            if ((result/10)==0)
+                break;
+            result /= 10;
+        }
+
+        for (int i=0;i<10;i++){
+            int count=0;
+            for (int j=0;j<10;j++){
+                if (i==num[j])
+                    count++;
+            }
+            fResult[i]=count;
+        }
+
+        for (int i=0;i<10;i++){
+            System.out.println(fResult[i]);
+        }
+        
+        scan.close();
+    }
+}
